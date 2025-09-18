57e822cccce6ae5d6af322fa27ad323250015bb3
Blackburn0 <hamzatadebayo5@gmail.com>
2025-09-18 07:58:59 +0100

    Add change password functionality with validation and update view

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterView, LoginView, PasswordResetRequestView, PasswordResetConfirmView
+from .views import RegisterView, LoginView, PasswordResetRequestView, PasswordResetConfirmView, ChangePasswordView
 
 urlpatterns = [
     path("signup/", RegisterView.as_view(), name="signup"),
@@ -8,4 +8,6 @@ urlpatterns = [
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("password/forgot/", PasswordResetRequestView.as_view(), name="password_reset_request"),
     path("password/reset/<uidb64>/<token>/", PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("change-password/", ChangePasswordView.as_view(), name="change_password"),
+
 ]
